e6deeb85d535a9576600e181d4c8f3ccdc25fe71
Tien Kim <tienkim9920@gmail.com>
2022-05-29 18:01:40 +0700

    update

--- controller/order-detail.controller.js
+++ controller/order-detail.controller.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const OrderDetail = require('../schema/order-detail.model');
+const Product = require('../schema/product.model');
 
 const router = express.Router();
 
@@ -17,6 +18,21 @@ router.post('/', async (req, res) => {
 
     res.json(orderDetail);
 
+})
+
+router.get('/:orderId', async (req, res) => {
+    const orderDetail = await OrderDetail.findAll({
+        where: { orderId: req.params.orderId },
+        include: [
+            { model: Product }
+        ]
+    })
+
+    try {
+        res.json(orderDetail);
+    }catch (err) {
+        res.json(err)
+    }
 })
 
 module.exports = router;
